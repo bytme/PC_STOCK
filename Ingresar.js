7ae314d6e39f3bdba96929cf62fb3175dbd426f9
bytme <90117724+bytme@users.noreply.github.com>
2021-10-23 17:44:14 -0500

    Archivo Java_Script
    Se crea Archivo Java Script

---
+++ Ingresar.js
@@ -0,0 +1,11 @@
+// funcion para ingresar pidiendo datos 
+
+function Ingresar(){
+    var usuario=document.getElementById("usuario").value;
+    var contrasena=document.getElementById("contrasena").value;
+
+    // imprimimos usuario y contraseña algo simple
+    // para demostrar como obtener datos de html y hacia javascript
+
+    alert("Su usuario es: "+usuario+" y su contraseña es: "+contrasena);
+}
